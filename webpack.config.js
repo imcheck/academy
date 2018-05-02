f69b06b35c4ae0e0bef91bcf826477e0975558d5
imcheck <sungwon520@gmail.com>
2018-05-03 01:19:04 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    `./src/app.js`
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [new UglifyJSPlugin()]
+}
