ca7ad2e06454c0c67874a355fcb2f618ece67a42
imCHECK <sungwon520@gmail.com>
2018-05-04 02:01:27 +0900

    Add styled-components

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ it helps to start a react-based application with simple settings.
 - `react v16`
 - `babel` supported for ES6, JSX
 - `webpack v4.0`
+- `styled-components`
 
 ## How to start
- - `npm run build`: makes `./dist/bundle.js`
+ - `npm start`: Run DevServer(ExpressJS)
+ - `npm run build`: Makes `./dist/bundle.js`
--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
+const Container = styled.div `
+  height: 100px;
+  background-color: orange;
+  color: yellow;
+`
 class App extends React.Component {
   render() {
-    return <div>Hello react!!!!!!!</div>;
+    return <Container>Hello React Using styled-components</Container>;
   }
 }
 ReactDOM.render(<App />, document.getElementById("root"));
