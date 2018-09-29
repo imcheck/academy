cc0d2283a5e75e61ec2d20c53d0909f71a7ccd61
sungwon.choi <sungwon.choi@devsisters.com>
2018-09-29 18:23:27 +0900

    Add a button for registering Student

--- src/containers/Student.js
+++ src/containers/Student.js
@@ -8,15 +8,24 @@ import ErrorHOC from '@hoc';
 import StudentTable from '@containers/StudentTable';
 import CommonLayout from '@layouts/CommonLayout';
 import Filter from '@containers/TableFilter';
+import { Button } from '@components';
 
 class Student extends React.Component {
   render() {
     return (
       <CommonLayout>
-        <Title>
-          <FontAwesomeIcon icon={faUserGraduate}/>
-          &nbsp;학생
-        </Title>
+        <Header>
+          <Title>
+            <FontAwesomeIcon icon={faUserGraduate}/>
+            &nbsp;학생
+          </Title>
+          <Container>
+            <Row>학생 추가, 수정, 검색기능을 할 수 있습니다.</Row>
+            <Row>
+              <Button height="30px" type="green">학생 추가</Button>
+            </Row>
+          </Container>
+        </Header>
         <Filter type="student"/>
         <StudentTable />
       </CommonLayout>
@@ -31,9 +40,21 @@ const mapDispatchToProps = (dispatch) => ({
 
 export default ErrorHOC(connect(mapStateToProps, mapDispatchToProps)(Student));
 
+const Header = styled.div`
+  height: 60px;
+  margin-bottom: 10px;
+`
 const Title = styled.div`
-  text-align: center;
   font-size: 48px;
   line-height: 48px;
-  margin-bottom: 10px;
+  float: left;
+`
+const Container = styled.div`
+  color: ${props => props.theme._components.gray}
+  float: left;
+  margin-left: 20px;
+`
+const Row = styled.div`
+  height: 30px;
+  line-height: 30px;
 `
